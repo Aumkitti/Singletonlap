f7c05f82c52b4e845ebdeeaa27a106c46810b346
Aumkitti <aumkitti.01@gmail.com>
2023-07-17 11:16:25 +0700

    Initial commit

---
+++ src/MathTestDriver.java
@@ -0,0 +1,15 @@
+public class MathTestDriver {
+public static void main(String[] args) {
+
+MathSingleton m1 = MathSingleton.getInstance();
+m1.setVariable1(15);
+m1.setVariable2(3);
+System.out.println(m1.add());
+
+MathSingleton m2 = MathSingleton.getInstance();
+System.out.println(m1.equals(m2));
+m2.setVariable2(5);
+System.out.println(m1.add());
+        
+    }
+}
